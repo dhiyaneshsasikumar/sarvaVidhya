f3512dc7066497d6b77bf7322d23f67797c2cf84
dhiyaneshsasikumar <dhiyaneshsasikumar@gmail.com>
2024-09-05 14:36:36 +0530

    Initial setup

---
+++ src/pages/servicePage/webDevelopment/WebDevelopment.jsx
@@ -0,0 +1,187 @@
+import React from 'react'
+import './webDevelopment.css'
+const WebDevelopment = () => {
+    return (
+        <div class="mainDiv">
+            <div class="serviceHeader">
+                <h2>Website Development</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod mollis tempus. Proin sapien
+                    augue, semper ac nibh vel.
+                </p>
+                <div class="buttonSection">
+                    <button class="btn1">Explore Now</button>
+                    <button class="btn2"><a href="../contactus/ContactUs.html">Contact Us</a></button>
+                </div>
+                <div class="marquee">
+                    <div class="track">
+                        <div class="content">Lorem ipsum dolor sit amet ✦ consectetur adipiscing elit ✦ Integer euismod
+                            mollis tempus ✦ Proin sapien
+                            augue ✦ semper ac nibh vel ✦ Lorem ipsum dolor sit amet ✦ consectetur adipiscing elit ✦
+                            Integer euismod mollis tempus ✦ Proin sapien
+                            augue ✦ semper ac nibh vel ✦ Lorem ipsum dolor sit amet ✦ consectetur adipiscing elit ✦
+                            Integer euismod mollis tempus ✦ Proin sapien
+                            augue ✦ semper ac nibh vel ✦ Lorem ipsum dolor sit amet ✦ consectetur adipiscing elit ✦
+                            Integer euismod mollis tempus ✦ Proin sapien
+                            augue ✦ semper ac nibh vel ✦</div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="section2">
+                <h2>Website Design and Development</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod mollis tempus. Proin sapien
+                    augue, semper ac nibh vel.</p>
+                <div class="responsive-grid">
+                    <div class="grid">
+                        <img src="/assets/Icons/code.png" alt="" />
+                        <div class="gridText">
+                            <h3>Lorem ipsum dolor sit amet</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod mollis tempus.
+                                Proin sapien
+                                augue, semper ac nibh vel.</p>
+                        </div>
+                    </div>
+                    <div class="grid">
+                        <img src="/assets/Icons/code.png" alt="" />
+                        <div class="gridText">
+                            <h3>Lorem ipsum dolor sit amet</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod mollis tempus.
+                                Proin sapien
+                                augue, semper ac nibh vel.</p>
+                        </div>
+                    </div>
+                    <div class="grid">
+                        <img src="/assets/Icons/code.png" alt="" />
+                        <div class="gridText">
+                            <h3>Lorem ipsum dolor sit amet</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod mollis tempus.
+                                Proin sapien
+                                augue, semper ac nibh vel.</p>
+                        </div>
+                    </div>
+                    <div class="grid">
+                        <img src="/assets/Icons/code.png" alt="" />
+                        <div class="gridText">
+                            <h3>Lorem ipsum dolor sit amet</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod mollis tempus.
+                                Proin sapien
+                                augue, semper ac nibh vel.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="section3Div">
+                <div class="section3 ">
+                    <div><img src="/assets/Computer troubleshooting-amico.png" alt="" /></div>
+                    <div class="right glass-effect">
+                        <div class="title ">
+                            <h3>Web Development & Design</h3>
+                            <hr />
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
+                            consectetur
+                            adipiscing elit. Lorem ips dolor sit amet, consectetur adipiscing elit.
+                            Lorem ipsum ing elit. dolor sit amet, consectetur adipiscing elit.
+                            Lorem ipsum dolor sit amet, consectetur adipisc
+                            Lorem ipsum dolor sit amet, consectetur adipiscing um dolor sit amet, consectetur adipiscing
+                            elit. Lorem ipsum dolor sit
+                            amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Lorem ipsum elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                        <button class="customButton">Get a Quote</button>
+                    </div>
+                </div>
+                <div class="gallery-container">
+                    <div class="gallery">
+                        <img src="/assets/carousel/1700486907837_tdy_news_8a_kreutz_mystery_dog_illness_231120_1920x1080-ggrpnf.webp"
+                            alt="Image 1" />
+                        <img src="/assets/carousel/201030094143-stock-rhodesian-ridgeback.jpg" alt="Image 2" />
+                        <img src="/assets/carousel/ASHH5bDmsp6wnK6mEfZdcU-1200-80.jpg" alt="Image 3" />
+                        <img src="/assets/carousel/Golden+Retrievers+dans+pet+care.jpeg" alt="Image 4" />
+                        <img src="/assets/carousel/image.png" alt="Image 5" />
+
+                        <img src="/assets/carousel/1700486907837_tdy_news_8a_kreutz_mystery_dog_illness_231120_1920x1080-ggrpnf.webp"
+                            alt="Image 1" />
+                        <img src="/assets/carousel/201030094143-stock-rhodesian-ridgeback.jpg" alt="Image 2" />
+                        <img src="/assets/carousel/ASHH5bDmsp6wnK6mEfZdcU-1200-80.jpg" alt="Image 3" />
+                        <img src="/assets/carousel/Golden+Retrievers+dans+pet+care.jpeg" alt="Image 4" />
+                        <img src="/assets/carousel/image.png" alt="Image 5" />
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="section4">
+
+                <div class="left glass-effect">
+                    <div class="title">
+                        <h3>Ecommerce Solutions</h3>
+                        <hr />
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit. Lorem ips dolor sit amet, consectetur adipiscing elit.
+                        Lorem ipsum ing elit. dolor sit amet, consectetur adipiscing elit.
+                        Lorem ipsum dolor sit amet, consectetur adipisc
+                        Lorem ipsum dolor sit amet, consectetur adipiscing um dolor sit amet, consectetur adipiscing
+                        elit. Lorem ipsum dolor sit
+                        amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Lorem ipsum elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                    <button class="customButton">Get a Quote</button>
+                </div>
+
+                <div><img src="/assets/Computer troubleshooting-amico.png" alt="" /></div>
+            </div>
+
+
+            <div class="section3Div">
+                <div class="section3">
+                    <div><img src="/assets/Computer troubleshooting-amico.png" alt="" /></div>
+                    <div class="right glass-effect">
+                        <div class="title">
+                            <h3>Website Revamp</h3>
+                            <hr />
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
+                            consectetur
+                            adipiscing elit. Lorem ips dolor sit amet, consectetur adipiscing elit.
+                            Lorem ipsum ing elit. dolor sit amet, consectetur adipiscing elit.
+                            Lorem ipsum dolor sit amet, consectetur adipisc
+                            Lorem ipsum dolor sit amet, consectetur adipiscing um dolor sit amet, consectetur adipiscing
+                            elit. Lorem ipsum dolor sit
+                            amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Lorem ipsum elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                        <button class="customButton">Get a Quote</button>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div class="section4">
+
+                <div class="left glass-effect">
+                    <div class="title">
+                        <h3>Website Maintenance</h3>
+                        <hr />
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit. Lorem ips dolor sit amet, consectetur adipiscing elit.
+                        Lorem ipsum ing elit. dolor sit amet, consectetur adipiscing elit.
+                        Lorem ipsum dolor sit amet, consectetur adipisc
+                        Lorem ipsum dolor sit amet, consectetur adipiscing um dolor sit amet, consectetur adipiscing
+                        elit. Lorem ipsum dolor sit
+                        amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Lorem ipsum elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                    <button class="customButton">Get a Quote</button>
+                </div>
+
+                <div><img src="/assets/Computer troubleshooting-amico.png" alt="" /></div>
+            </div>
+
+        </div>
+    )
+}
+
+export default WebDevelopment
